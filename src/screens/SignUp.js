c3d27b07373c6b20d3a7f95bb0c0778c43abe029
zipperdev <sh20101119@gmail.com>
2021-07-20 20:41:27 +0900

    User Interface Commit 1.9

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { gql, useMutation } from "@apollo/client";
@@ -41,9 +41,29 @@ const CREATE_ACCOUNT_MUTATION = gql`
     }
 `;
 
+const VERIFY_EMAIL_MUTATION = gql`
+    mutation verifyEmail(
+        $email: String!
+        $verifyCode: String!
+    ) {
+        verifyEmail(
+            email: $email
+            verifyCode: $verifyCode
+        ) {
+            success
+            error
+        }
+    }
+`;
+
 function SignUp() {
     const history = useHistory();
+    const [ section, setSection ] = useState(1);
+    const [ userData, setUserData ] = useState();
     const { register, handleSubmit, errors, formState, setError, getValues, clearErrors } = useForm({
+        mode: "onChange"
+    });
+    const verify = useForm({
         mode: "onChange"
     });
     const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
@@ -55,7 +75,23 @@ function SignUp() {
                     message: error
                 });
             } else {
-                history.push(routes.home, { message: "Account created. Please log in.", email, password });
+                setUserData({
+                    email, 
+                    password
+                });
+                setSection(2);
+            };
+        }
+    });
+    const [verifyEmail, { loading: verifyLoading }] = useMutation(VERIFY_EMAIL_MUTATION, {
+        onCompleted: data => {
+            const { verifyEmail: { success, error } } = data;
+            if (!success) {
+                return verify.setError("result", {
+                    message: error
+                });
+            } else {
+                history.push(routes.home, { message: "Account created and verified. Please log in.", email: userData.email, password: userData.password });
             };
         }
     });
@@ -93,40 +129,65 @@ function SignUp() {
             <TopBox>
                 <HeaderContainer>
                     <LogoImage />
-                    <Subtitle>Sign up to see photos and videos from your friends.</Subtitle>
+                    <Subtitle>{section === 1 ? "Sign up to see photos and videos from your friends." : "Sent the email. Please check the email and type the 6 digit code."}</Subtitle>
                 </HeaderContainer>
-                <form onSubmit={handleSubmit(onSubmitValid)}>
-                    <Input onChange={clearLoginError} ref={register({
-                        required: "First name is required.",
-                        validate: currrentValue => currrentValue.length < 40
-                    })} errorMessage={errors?.firstName?.type === "validate" ? "First name must be shorter than 40." : errors?.firstName?.message} name="firstName" type="text" placeholder="First Name" />
-                    <Input onChange={clearLoginError} ref={register({
-                        validate: currrentValue => currrentValue.length < 40
-                    })} errorMessage={errors?.lastName?.type === "validate" ? "Last name must be shorter than 40." : errors?.lastName?.message} name="lastName" type="text" placeholder="Last Name" />
-                    <Input onChange={clearLoginError} ref={register({
-                        required: "Username is required.",
-                        validate: currrentValue => currrentValue.length < 40
-                    })} errorMessage={errors?.username?.type === "validate" ? "Username must be shorter than 40." : errors?.username?.message} name="username" type="text" placeholder="Username" />
-                    <Input onChange={clearLoginError} ref={register({
-                        required: "Email address is required.",
-                        pattern: {
-                            value: /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i,
-                            message: "Email address is invalid."
-                        }
-                    })} errorMessage={errors?.email?.message} name="email" type="text" placeholder="Email" />
-                    <Input onChange={clearLoginError} ref={register({
-                        required: "Password is required.",
-                        validate: currentValue => currentValue.length >= 8
-                    })} errorMessage={errors?.password?.type === "validate" ? "Password must be longger than 7." : errors?.password?.message} name="password" type="password" placeholder="Password" />
-                    <Input onChange={clearLoginError} ref={register({
-                        required: "Password confrimation is required.",
-                        validate: currrentValue => {
-                            const { password } = getValues();
-                            return currrentValue === password;
-                        }
-                    })} errorMessage={errors?.confrimPassword?.type === "validate" ? "Password confrimation doesn't match." : errors?.confrimPassword?.message} name="confrimPassword" type="password" placeholder="Password Confrim" />
-                    <Button errorResult={errors?.result?.message} type="submit" disabled={!formState.isValid || loading}>{loading ? "Loading..." : "Sign Up"}</Button>
-                </form>
+                {section === 1 ? (
+                    <form onSubmit={handleSubmit(onSubmitValid)}>
+                        <Input onChange={clearLoginError} ref={register({
+                            required: "First name is required.",
+                            validate: currrentValue => currrentValue.length < 40
+                        })} errorMessage={errors?.firstName?.type === "validate" ? "First name must be shorter than 40." : errors?.firstName?.message} name="firstName" type="text" placeholder="First Name" />
+                        <Input onChange={clearLoginError} ref={register({
+                            validate: currrentValue => currrentValue.length < 40
+                        })} errorMessage={errors?.lastName?.type === "validate" ? "Last name must be shorter than 40." : errors?.lastName?.message} name="lastName" type="text" placeholder="Last Name" />
+                        <Input onChange={clearLoginError} ref={register({
+                            required: "Username is required.",
+                            validate: currrentValue => currrentValue.length < 40
+                        })} errorMessage={errors?.username?.type === "validate" ? "Username must be shorter than 40." : errors?.username?.message} name="username" type="text" placeholder="Username" />
+                        <Input onChange={clearLoginError} ref={register({
+                            required: "Email address is required.",
+                            pattern: {
+                                value: /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i,
+                                message: "Email address is invalid."
+                            }
+                        })} errorMessage={errors?.email?.message} name="email" type="text" placeholder="Email" />
+                        <Input onChange={clearLoginError} ref={register({
+                            required: "Password is required.",
+                            validate: currentValue => currentValue.length >= 8
+                        })} errorMessage={errors?.password?.type === "validate" ? "Password must be longger than 7." : errors?.password?.message} name="password" type="password" placeholder="Password" />
+                        <Input onChange={clearLoginError} ref={register({
+                            required: "Password confrimation is required.",
+                            validate: currrentValue => {
+                                const { password } = getValues();
+                                return currrentValue === password;
+                            }
+                        })} errorMessage={errors?.confrimPassword?.type === "validate" ? "Password confrimation doesn't match." : errors?.confrimPassword?.message} name="confrimPassword" type="password" placeholder="Password Confrim" />
+                        <Button errorResult={errors?.result?.message} type="submit" disabled={!formState.isValid || loading}>{loading ? "Loading..." : "Go To Verify"}</Button>
+                    </form>
+                ) : (
+                    <>
+                        <form onSubmit={verify.handleSubmit(data => {
+                            if (verifyLoading) {
+                                return;
+                            } else {
+                                const {
+                                    verifyCode
+                                } = data;
+                                verifyEmail({
+                                    variables: {
+                                        verifyCode, 
+                                        email: userData.email
+                                    }
+                                });
+                            };
+                        })}>
+                            <Input onChange={() => verify.clearErrors("result")} ref={verify.register({
+                                required: "Verify code is required."
+                            })} errorMessage={verify.errors?.verifyCode?.message} name="verifyCode" type="text" placeholder="Verify Code (6 Digit)" />
+                            <Button errorResult={verify.errors?.result?.message} type="submit" disabled={!verify.formState.isValid || verifyLoading}>{verifyLoading ? "Loading..." : "Sign Up"}</Button>
+                        </form>
+                    </>
+                )}
             </TopBox>
             <BottomBox actionText="Already have an acount?" link={routes.home} linkText="Log In" />
         </AuthLayout>
